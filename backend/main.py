29eeaab85692d7201f2ce2647d4f29bbfc151fa2
kwiatkowskim <kwiatkowski.michal.2003@gmail.com>
2025-05-10 18:55:26 +0200

    reqs

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
-from routes.flashcards import router as flashcards_router
+from backend.routes.flashcards import router as flashcards_router
+import uvicorn
 
 app = FastAPI()
 
 # Include the flashcards router under /api
 app.include_router(flashcards_router, prefix="/api")
+
+if __name__ == "main":
+    uvicorn.run(
+        "main:app",
+        host="127.0.0.1",
+        port=8001,
+        reload=True
+    )
--- backend/routes/flashcards.py
+++ backend/routes/flashcards.py
@@ -1,4 +1,7 @@
 from fastapi import APIRouter
+
+from backend.data.mocked_data import flashcards
+
 router = APIRouter()
 
 @router.get("/get_flashcards")
@@ -6,6 +9,6 @@ def get_flashcards():
     return flashcards
 
 
-@router.get("/get_flashcards_history")
-def get_flashcards_history():
-    return flashcards_history
+# @router.get("/get_flashcards_history")
+# def get_flashcards_history():
+#     return flashcards_history
